d2d65f68187a80828b304aa164d37c0687729f60
Devloop7 <maor.oh.bb@gmail.com>
2026-07-09 14:35:21 +0300

    Delete scriptedelite directory

--- scriptedelite/core/__init__.py
+++
@@ -1,4 +0,0 @@
-# ScriptedElite AI - Core package
-# Imports are done explicitly in main.py to avoid requiring all deps at package load time.
-
-__all__ = ["AppConfig", "ScreenCapture", "YOLODetector", "AimController"]
